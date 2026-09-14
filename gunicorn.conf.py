81c89858471db91798c758714931683264caf1b1
amantilla <mantillaalberto11@gmail.com>
2026-09-14 18:18:06 -0500

    Puerto 8001 en gunicorn para no chocar con analitica

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
-bind = "127.0.0.1:8000"
+bind = "127.0.0.1:8001"
 workers = min(multiprocessing.cpu_count() * 2 + 1, 4)
 worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 120
